a09a1c59d341034acaa275819bf7ce360d3c1636
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-10-16 16:44:44 -0400

    Updated Urls

--- backend/settings.py
+++ backend/settings.py
@@ -91,7 +91,7 @@ environ.Env.read_env()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     },
     # 'remote': {
     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
